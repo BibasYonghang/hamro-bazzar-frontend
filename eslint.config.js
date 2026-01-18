8a6d6c97dba2dc0724df18fa68c6fca678733053
BibasYonghang <bibasyonghang100@gmail.com>
2026-01-18 17:22:50 +0545

    ci: add frontend CI pipeline and backend CD workflow

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+/** eslint.config.js */
+export default [
+  {
+    files: ["*.js", "*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+    rules: {
+      semi: ["error", "always"],
+      "no-console": "off",
+    },
+  },
+];
